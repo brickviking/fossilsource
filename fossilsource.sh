498514f84df46d55ad27df7498213ff4c7601b88
brickviking <brickviking@gmail.com>
2022-08-12 16:22:02 +1200

    Version 0.2, correcting for no sqlite-book

--- fossilsource.sh
+++ fossilsource.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 # v0.1 iterate through my fossils
+# v0.2 Add in some git projects
 # Really needs to be run from the source directory first
 
-FIRSTHOME= ( fossil "/home/viking/src/c"
-git "/home/viking/src/c"
-git "/h3/viking/src/c++/hosts"
-)
+#FIRSTHOME= ( fossil "/home/viking/src/c"
+#git "/home/viking/src/c"
+#git "/h3/viking/src/c++/hosts"
+#)
 
 # Takes path arg
 function fossilstuff() {
 	pushd /home/viking/src/c
 	for t in fossil-scm sqlite; do  # want subshell - saves us a pushd/popd
+		case ${t} in "fossil-scm")
+			cd ${t}
+			fossil pull -R ${t%%-scm}.fossil
+			fossil pull -R ${t%%-scm}forum.fossil 
+			fossil pull -R ${t%%-scm}-book.fossil 
+			cd ..
+		;;
+		"sqlite") 
+			cd ${t}
 			fossil pull -R ${t%%-scm}.fossil
 			fossil pull -R ${t%%-scm}forum.fossil 
 			cd ..
+		;;
+		esac		
 	done
 	popd
 }
